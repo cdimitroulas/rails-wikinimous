2e46dd011dd0a6ec3116be61a02a6795b2248f86
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-08 17:46:12 +0000

    fixed some bugs with the login logic which occurred after I changed the way the form was generated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-
+    @user = User.find_by_email(params[:user][:email])
     # Checks if the user exists and if the password is correct
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
