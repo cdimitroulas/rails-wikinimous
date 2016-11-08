c9b9a0db45c136769091941764bcc582ee94a790
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-08 16:21:17 +0000

    Created a user controller which allows the creation of a new user through the signup page. Added the bcrypt gem to the Gemfile so that passwords can be secure (User model is using has_secure_password method)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  # Routes for navigating the articles and CRUD
   resources :articles
   root to: "articles#index"
+
+
+  # Routes for signup
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 end
